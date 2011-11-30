0beb05eaacfca9300d19d6acb6bba2f8821571ff
Balázs Antal <balazs.antal@pixelephant.hu>
2011-11-30 10:45:05 +0100

    login 2.0

--- traveltrix_admin/login.php
+++ traveltrix_admin/login.php
@@ -8,25 +8,28 @@
 ?>
 <html>
 <head>
-
+	<link href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css" rel="stylesheet">
+	<link href="lib/css/style.css" rel="stylesheet">
 </head>
 <body>
 
-<div id="login">
-	<p>Bejelentkezés</p>
-	<form action="lib/php/login_process.php" method="POST">
-		<input type="hidden" name="token" id="token" value="<?php echo $token;?>" />
-		<input type="hidden" name="action" id="action" value="login" />
-		<div class="row">
-			<label for="email">E-mail</label><input type="text" id="email" name="email" /><span style="display:none;" class="error">Hiba!</span>
-		</div>
-		<div class="row">
-			<label for="password">Jelszó</label><input type="password" id="password" name="password" /><span style="display:none;" class="error">Hiba!</span>
-		</div>
-		<div class="row">
-			<input type="submit" value="Elküld" />
-		</div>
-	</form>
+<div id="content-inner">
+	<div id="login">
+		<h1>Bejelentkezés</h1>
+		<form action="lib/php/login_process.php" method="POST">
+			<input type="hidden" name="token" id="token" value="<?php echo $token;?>" />
+			<input type="hidden" name="action" id="action" value="login" />
+			<div class="formrow">
+				<label for="email">E-mail</label><input type="text" id="email" name="email" /><span style="display:none;" class="error">Hiba!</span>
+			</div>
+			<div class="formrow">
+				<label for="password">Jelszó</label><input type="password" id="password" name="password" /><span style="display:none;" class="error">Hiba!</span>
+			</div>
+			<div class="formrow">
+				<input class="btn primary" type="submit" value="Elküld" />
+			</div>
+		</form>
+	</div>
 </div>
 </body>
 </html>
